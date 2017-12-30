c4227a4234bf3bfb93426e151bfbec4f2fc132b6
Tom von Clef <tom@vonclef.com>
2017-12-29 19:32:38 -0500

    Finished Part 2 of Day 2

--- day2_part2.hs
+++ day2_part2.hs
@@ -1,31 +1,24 @@
 -- Part 1
 
 import System.Environment   
-import Data.List
 import Data.List.Split
 
--- Lazy list of primes from https://stackoverflow.com/a/3596536
-primes :: [Integer]
-primes = sieve [2..]
-  where
-    sieve (p:xs) = p : sieve [x|x <- xs, x `rem` p > 0]
+quot' :: Integer -> [Integer] -> Integer
+quot' x (y:rest)
+    | x `rem` y == 0    = x `quot` y
+    | y `rem` x == 0    = y `quot` x
+    | null rest         = 0
+    | otherwise         = quot' x rest
 
---isFactorOf :: Integer -> Integer -> Bool
---isFactorOf mightBeFactor number = number `rem` mightBeFactor == 0
+-- Takes 
+quotientOfDivisiblePair :: [Integer] -> Integer
+quotientOfDivisiblePair (x:rest)
+    | quotX /=0 = quotX
+    | otherwise = quotientOfDivisiblePair rest
+    where quotX = quot' x rest
 
-firstFactorIn :: Integer -> [Integer] -> Integer
-firstFactorIn x xs = do
-    let maybeFound = find (\potentialFactor -> x `rem` potentialFactor == 0) xs
-    maybe 0 id maybeFound
-
-primeFactors :: Integer -> [Integer]
-primeFactors x
-    | x == 0 || x == 1 = []
-    | otherwise = firstFactor : primeFactors (x `quot` firstFactor)
-    where firstFactor = firstFactorIn x primes
-
-uniquePrimeFactors :: Integer -> [Integer]
-uniquePrimeFactors x = nub (primeFactors x)
+spreadsheetHash :: [[Integer]] -> Integer
+spreadsheetHash spreadsheet = sum $ map quotientOfDivisiblePair spreadsheet
 
 convertToInteger :: String -> Integer
 convertToInteger x = read x :: Integer
@@ -35,14 +28,6 @@ parseSpreadsheet string = do
     let rowStrings = filter (/= "") (splitOn "\n" string)
     let columnStrings = map (splitOneOf " \t") rowStrings
     map (map convertToInteger) columnStrings
-
-spreadsheetHash :: [[Integer]] -> Integer
-spreadsheetHash spreadsheet = do
-
-    let spreadsheetMaxs = map maximum spreadsheet
-    let spreadsheetMins = map minimum spreadsheet
-    let spreadsheetDiffs = zipWith (-) spreadsheetMaxs spreadsheetMins
-    sum spreadsheetDiffs
 
 main :: IO ()
 main = do
