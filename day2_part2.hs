66ff4283d5e84ee24ae822a000c010b68d9178a8
Tom von Clef <tom@vonclef.com>
2017-12-29 19:50:17 -0500

    cleaned up solution

--- day2_part2.hs
+++ day2_part2.hs
@@ -1,33 +1,28 @@
--- Part 1
+-- Part 2
 
 import System.Environment   
 import Data.List.Split
 
-quot' :: Integer -> [Integer] -> Integer
-quot' x (y:rest)
+evenlyDivisibleQuotient :: Integer -> [Integer] -> Integer
+evenlyDivisibleQuotient x (y:rest)
     | x `rem` y == 0    = x `quot` y
     | y `rem` x == 0    = y `quot` x
     | null rest         = 0
-    | otherwise         = quot' x rest
+    | otherwise         = evenlyDivisibleQuotient x rest
 
--- Takes 
 quotientOfDivisiblePair :: [Integer] -> Integer
-quotientOfDivisiblePair (x:rest)
-    | quotX /=0 = quotX
-    | otherwise = quotientOfDivisiblePair rest
-    where quotX = quot' x rest
+quotientOfDivisiblePair (x:rest) = do
+    let quotX = evenlyDivisibleQuotient x rest
+    if quotX /=0 then quotX else quotientOfDivisiblePair rest
 
 spreadsheetHash :: [[Integer]] -> Integer
 spreadsheetHash spreadsheet = sum $ map quotientOfDivisiblePair spreadsheet
-
-convertToInteger :: String -> Integer
-convertToInteger x = read x :: Integer
 
 parseSpreadsheet :: String -> [[Integer]]
 parseSpreadsheet string = do
     let rowStrings = filter (/= "") (splitOn "\n" string)
     let columnStrings = map (splitOneOf " \t") rowStrings
-    map (map convertToInteger) columnStrings
+    map (map read) columnStrings
 
 main :: IO ()
 main = do
